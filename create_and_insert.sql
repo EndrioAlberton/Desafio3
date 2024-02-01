c3a32a4ba2dea4c1e2b282c3df40d1365499f7e7
EndrioAlberton <endrio.alberton@gmail.com>
2024-02-01 17:55:08 -0300

    Initial commit

---
+++ create_and_insert.sql
@@ -0,0 +1,12 @@
+USE challenge3;
+
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    value DECIMAL(10, 2) NOT NULL
+);
+
+INSERT INTO products (name, description, value) VALUES ('Product 1', 'Description for Product 1', 10.50);
+INSERT INTO products (name, description, value) VALUES ('Product 2', 'Description for Product 2', 15.75);
+INSERT INTO products (name, description, value) VALUES ('Product 3', 'Description for Product 3', 20.00);
