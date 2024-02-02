37ab7ae504856e4d12613584a8254221531c0e91
Marcos <abreumarcos13462@gmail.com>
2024-02-02 20:28:14 -0300

    Insert product test

--- src/application/eCommerceaApplication.java
+++ src/application/eCommerceaApplication.java
@@ -1,36 +1,17 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import db.DB;
+import model.dao.DaoFactory;
+import model.dao.ProductDao;
+import model.entities.Product;
 
 public class eCommerceaApplication {
     public static void main(String[] args) {
 
-		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
-		try {
-			conn = DB.getConnection();
-	
-			st = conn.createStatement();
-			
-			rs = st.executeQuery("select * from products");
-			
-			while (rs.next()) {
-				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-			}
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			DB.closeResultSet(rs);
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		ProductDao productDao = DaoFactory.createProductDao();
+
+		System.out.println("Test Insert product");
+		Product newProduct = new Product( "Product name", "Description", 150.0, 2);
+		productDao.insert(newProduct);
+		System.out.println("Inserted! New id = " + newProduct.getId());
 	}
 }
