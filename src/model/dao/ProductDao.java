2181a85ea2b2d6868f95c6b9cb598df883985ff8
Carolaine Machado <carolainemachado12345@gmail.com>
2024-02-06 22:59:52 -0300

    Object created in productdao class

--- src/model/dao/ProductDao.java
+++ src/model/dao/ProductDao.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 import model.entities.Product;
 
 public interface ProductDao {
-
+	/*responsavel por inserir ... no banco de dados,
+	este objeto a ser enviado como parametro de entrada 
+	 */
     void createProduct(Product obj);
     void updateProduct(Product obj);
     void deleteById(Integer id);
